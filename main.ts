d998e956fdb08affd7ce11bf8906c92f7b2b42ef
evagrc19 <103173881+evagrc19@users.noreply.github.com>
2022-05-17 13:16:33 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,12 +7,28 @@ basic.forever(function () {
             . . # . .
             . . # . .
             `)
-    } else if (input.acceleration(Dimension.Y) < 100) {
+    } else if (input.acceleration(Dimension.Y) > 100) {
         basic.showLeds(`
             . . # . .
             . . # . .
             # . # . #
             . # # # .
+            . . # . .
+            `)
+    } else if (input.acceleration(Dimension.X) < -100) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (input.acceleration(Dimension.X) > 100) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
             . . # . .
             `)
     } else {
